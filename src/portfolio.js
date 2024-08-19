f142b715de9a96f284bca6fc491b0ef60292cd5d
hjabbot <42661345+hjabbot@users.noreply.github.com>
2024-08-19 12:12:23 +0100

    Updated lottie josns

--- src/portfolio.js
+++ src/portfolio.js
@@ -33,9 +33,9 @@ const greeting = {
 // Social Media Links
 
 const socialMediaLinks = {
-  github: "https://github.com/saadpasta",
+  github: "https://github.com/hjabbot",
   linkedin: "https://www.linkedin.com/in/saadpasta/",
-  gmail: "saadpasta70@gmail.com",
+  gmail: "h.j.abbot@gmail.com",
   gitlab: "https://gitlab.com/saadpasta",
   facebook: "https://www.facebook.com/saad.pasta7",
   medium: "https://medium.com/@saadpasta",
