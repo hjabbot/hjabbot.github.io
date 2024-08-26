2d20cbcca4dfae303132b67b1c8ab289a4e444dd
hjabbot <42661345+hjabbot@users.noreply.github.com>
2024-08-26 13:45:30 +0100

    Confirmation message added on send

--- src/containers/contact/Contact.js
+++ src/containers/contact/Contact.js
@@ -6,14 +6,32 @@ import {Fade} from "react-reveal";
 import email from "../../assets/lottie/email";
 import DisplayLottie from "../../components/displayLottie/DisplayLottie";
 import StyleContext from "../../contexts/StyleContext";
+import Swal from 'sweetalert2';
 
-function ContactForm() {
+const ContactForm = () => {
+  const sentMessage = () => {
+    Swal.fire({
+      title: "Message sent!",
+      text: "Time to sit back and relax",
+      icon: "success"
+    });
+  }
   return (
   <div>
-    <form name="gform" id="gform" enctype="text/plain" action="https://docs.google.com/forms/d/e/1FAIpQLSc0L5nARtruFQ_QKonMj-RNK9TFUM4MFb1rUMrG2O-WwcqDGg/formResponse?" target="hidden_iframe" onsubmit="submitted=true;">
-      Name:<br /><input type="text" name="entry.2069977254" /><br />
-      Email address:<br /><input type="text" name="entry.332319714" /> <br />
-      How can I help?<br /><input type="text" name="entry.618086409" />
+    <form name="gform" id="gform" encType="text/plain" action="https://docs.google.com/forms/d/e/1FAIpQLSc0L5nARtruFQ_QKonMj-RNK9TFUM4MFb1rUMrG2O-WwcqDGg/formResponse?" target="hidden_iframe" onSubmit={sentMessage}>
+      <h2>Contact Me</h2>
+      <div name="input-box">
+        <label>Full Name</label>
+        <input type="text" name="entry.2069977254" className="field" placeholder="Enter your name" required />
+      </div>
+      <div name="input-box">
+        <label>Email Address</label>
+        <input type="email" name="entry.332319714" className="field" placeholder="Enter your email address" required />
+      </div>
+      <div name="input-box">
+        <label>How can I help?</label>
+        <textarea type="text" name="entry.618086409" className="field mess" placeholder="Enter your message" required />
+      </div>
       <input type="submit" value="Submit"></input>
       {/* <button type="submit">Send</button> */}
     </form>
